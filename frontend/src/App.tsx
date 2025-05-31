3b10d8c09824e8ed7d40c3e331da1a8c092e3f0f
wms198 <wang.minshi@icloud.com>
2025-05-31 18:15:21 +0200

    created frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,88 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+<div className="container-fluid">
+    <div className="row flex-nowrap">
+        <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
+            <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
+                <a href="/" className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                    <span className="fs-5 d-none d-sm-inline">Menu</span>
+                </a>
+                <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                    <li className="nav-item">
+                        <a href="#" className="nav-link align-middle px-0">
+                            <i className="fs-4 bi-house"></i> <span className="ms-1 d-none d-sm-inline">Home</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#submenu1" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-speedometer2"></i> <span className="ms-1 d-none d-sm-inline">Dashboard</span> </a>
+                        <ul className="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                            <li className="w-100">
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1 </a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2 </a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-table"></i> <span className="ms-1 d-none d-sm-inline">Orders</span></a>
+                    </li>
+                    <li>
+                        <a href="#submenu2" data-bs-toggle="collapse" className="nav-link px-0 align-middle ">
+                            <i className="fs-4 bi-bootstrap"></i> <span className="ms-1 d-none d-sm-inline">Bootstrap</span></a>
+                        <ul className="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
+                            <li className="w-100">
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 1</a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Item</span> 2</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#submenu3" data-bs-toggle="collapse" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-grid"></i> <span className="ms-1 d-none d-sm-inline">Products</span> </a>
+                            <ul className="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
+                            <li className="w-100">
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 1</a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 2</a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 3</a>
+                            </li>
+                            <li>
+                                <a href="#" className="nav-link px-0"> <span className="d-none d-sm-inline">Product</span> 4</a>
+                            </li>
+                        </ul>
+                    </li>
+                    <li>
+                        <a href="#" className="nav-link px-0 align-middle">
+                            <i className="fs-4 bi-people"></i> <span className="ms-1 d-none d-sm-inline">Customers</span> </a>
+                    </li>
+                </ul>
+                
+            </div>
+        </div>
+        <div className="py-3 mx-auto btn-primary  d-flex align-items-center flex-column" style={{width: '50%'}}>
+          <p className="lead">Hi. I'm here.fhsdkfhsjkfhkjdhfsjhfsjkdhfdjkshfdskhkdfdfdhksjhfkds
+            shdfkshjkjfhsdjfhsjkhfdjkshfjkshfjksdjfhjkshf
+            fdhshfkdjhfkjdhfjkshfjkdshfjkdshfkjhsdfjsfjlksjflksdjfksjfksjfkljsfkldsjfkljslkfjdklfjs
+          </p>
+          <div className="input-group ">
+            <textarea className="form-control custom-control" rows={4} cols={72}> </textarea>
+            <span className="input-group-addon btn btn-primary d-flex align-items-center">↑</span>
+          </div>
+        </div>
+    </div>
+</div>
+  );
+}
+
+export default App;
