96b7b5228590a5c7e55f2fe92223f16208c6cb5a
wms198 <wang.minshi@icloud.com>
2025-06-05 15:18:43 +0200

    modified siedebar file

--- frontend/src/Sidebar.tsx
+++ frontend/src/Sidebar.tsx
@@ -14,29 +14,25 @@ const Sidebar: React.FC = () => {
     error,
     isPending,
     data: appointments,
-  } = useFetch<Appointments[]>("http://localhost:8000/appointment/all");
+  } = useFetch<Appointments[]>("http://localhost:8000/appointment/");
 
 
 
 
 
   return (
-    <div className="col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-dark">
-      <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-2 text-white min-vh-100">
-        <a
-          href="/"
-          className="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none"
-        >
-          <span className="fs-5 d-none d-sm-inline">See YOU</span>
-        </a>
-
-        <button className="btn btn-primary my-2">Make an appointment</button>
+    <div className="col-xl-2 px-sm-2 px-0 bg-dark min-vh-100">
+      <div className="d-flex flex-column  align-items-sm-start px-3 pt-2 text-white ">
+          <span className="fs-3">See YOU</span>
+{/* <button className="btn btn-primary my-2">Make an appointment</button> */}
         {error && <div>{error}</div>}
         {isPending && <div>Loading...</div>}
         {appointments && <AppointmentList appointments={appointments} />}
       </div>
-      <AppointmentForm />
-    </div>
+      <div>
+        <AppointmentForm />
+      </div>
+    </div>   
   );
 };
 
