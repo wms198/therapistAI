7e735671f2f46e015938d197723cff4616826868
wms198 <wang.minshi@icloud.com>
2025-06-05 07:13:06 +0200

    changed UI

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,3 +1,8 @@
+
 export interface UserMessage {
     Message: string
+}
+
+export interface Appointments {
+    start_at: string
 }
--- routers/appointment.py
+++ routers/appointment.py
@@ -23,6 +23,14 @@ async def getLatest_appointment(session: Session = Depends(get_session)):
     details_of_appointment = session.exec(query).first()
     return details_of_appointment
 
+@router.get("/appointment/all", tags=["appointment"])
+async def getAll_appointments(session: Session = Depends(get_session)):
+    # https://www.geeksforgeeks.org/get-current-date-and-time-using-python/
+    query = select(Appointment).where(Appointment.start_at > datetime.datetime.now())
+    all_appointments = session.exec(query).all()
+    return all_appointments
+
+
 @router.patch("/appointment/{id}", tags=["appointment"])
 async def update_appointment(appointment_req:AppointmentRequest, id, session: Session = Depends(get_session)):
     appointment = session.get(Appointment, id)
