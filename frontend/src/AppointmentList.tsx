784bcd0de9d0e22dc23eba157c9f265d2c2fee06
wms198 <wang.minshi@icloud.com>
2025-06-05 09:50:21 +0200

    removed T and Z in datetime string

--- frontend/src/AppointmentList.tsx
+++ frontend/src/AppointmentList.tsx
@@ -8,7 +8,7 @@ const AppointmentList = ({appointments}: Props) => {
     return(
         <ul className="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start">
             {appointments.map((appointment) => (
-                <li>{appointment.start_at}</li>
+                <li>{appointment.start_at.replace("T", " ").replace("Z", "")}</li>
             ))}
         </ul>
     );
