bebd8aed7d90442f4e3ac8ea1cfed4972d6a166d
wms198 <wang.minshi@icloud.com>
2026-02-10 09:57:27 +0100

    Modified model

--- backend/therapistai/ai/__init__.py
+++ backend/therapistai/ai/__init__.py
@@ -6,7 +6,7 @@ SERVER_API_HOST = "127.0.0.1:1234"
 # This must be the *first* convenience API interaction (otherwise the SDK
 # implicitly creates a client that accesses the default server API host)
 lms.configure_default_client(SERVER_API_HOST)
-model = lms.llm("llama-3.2-3b-instruct")
+model = lms.llm("lmstudio-community/gemma-3-1B-it-qat-GGUF")
 
 SYSTEM_PROMP = """You are a personal therapist.
 You will get messages from the user from past conversations.
