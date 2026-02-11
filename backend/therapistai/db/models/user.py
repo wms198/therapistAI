60e0c126b3fdb89dffefa026496e1c9b87e58f20
wms198 <wang.minshi@icloud.com>
2026-02-11 11:28:19 +0100

    Added column

--- backend/therapistai/db/models/user.py
+++ backend/therapistai/db/models/user.py
@@ -1,8 +1,18 @@
 from sqlmodel import Field, SQLModel
 
-class User(SQLModel, table = True):
+
+# small wrapper around field and sa columns kwargs
+# https://sqlmodel.tiangolo.com/tutorial/indexes/#declare-indexes-with-sqlmodel
+# https://github.com/fastapi/sqlmodel/issues/82#issuecomment-1005858022
+# email: str = Field(sa_column_kwargs={"unique": True, "index": True})
+def AField(*args, **kwargs):
+    return Field(*args, sa_column_kwargs=kwargs)
+
+
+class User(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
-    firstName: str 
-    lastName: str 
-    email: str
+    firstName: str
+    lastName: str
+    email: str = AField(unique=True, index=True)
     emailProvider: str
+    password: str
