dc31eaee2fa3b092118d4082670b67367433f519
wms198 <wang.minshi@icloud.com>
2026-02-23 09:59:24 +0100

    Added test sample

---
+++ backend/therapistai/t_test.py
@@ -0,0 +1,20 @@
+from therapistai import t
+import pytest
+
+# pytest t_test.py -v
+# Funtion based
+@pytest.mark.parametrize(
+    ('input_n', 'expected'),
+    (
+        (5, 25),
+        (3., 9.),
+    )
+)
+def test_square(input_n, expected):
+    assert t.square(input_n,) == expected
+
+# def test_square():
+#     assert t.square(5) == 25
+
+# def test_square_float():
+#     assert t.square(3.) == 9.
