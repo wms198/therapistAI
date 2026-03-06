98541bec77e8d9026b3b7e1582ecd85f0206b7e9
wms198 <wang.minshi@icloud.com>
2026-03-06 14:02:34 +0100

    Modified DB_URL

--- backend/therapistai/db/__init__.py
+++ backend/therapistai/db/__init__.py
@@ -1,8 +1,9 @@
+import os
 from sqlalchemy import create_engine
 from sqlmodel import Session
 
 # https://www.geeksforgeeks.org/python/fastapi-crud-operations/
-engine = create_engine('postgresql+psycopg2://postgres:admin123@localhost:54321/therapistai')
+engine = create_engine(os.getenv("DB_URL"))
 
 def get_session():
     with Session(engine) as session:
