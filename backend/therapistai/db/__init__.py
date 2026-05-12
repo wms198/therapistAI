61ed48ebc4cda6c25412c36ca34a7263114cbabd
wms198 <wang.minshi@icloud.com>
2026-05-12 23:08:30 +0200

    use vercel compatible name

--- backend/therapistai/db/__init__.py
+++ backend/therapistai/db/__init__.py
@@ -3,8 +3,8 @@ from sqlalchemy import create_engine
 from sqlmodel import Session
 
 # https://www.geeksforgeeks.org/python/fastapi-crud-operations/
-engine = create_engine(os.getenv("DB_URL"))
-print(f"DB_URL: {os.getenv('DB_URL')}")
+engine = create_engine(os.getenv("DATABASE_URL"))
+print(f"DATABASE_URL: {len(os.getenv('DATABASE_URL'))}")
 
 def get_session():
     with Session(engine) as session:
