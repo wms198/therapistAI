79103e0a70d128cd5f6c9c789467705b14016c16
wms198 <wang.minshi@icloud.com>
2026-06-09 13:59:18 +0200

    Improve code maintainable

--- backend/therapistai/ai/__init__.py
+++ backend/therapistai/ai/__init__.py
@@ -7,11 +7,16 @@ LMS_API_HOST = os.getenv('LMS_API_HOST', "127.0.0.1:1234")
 # This must be the *first* convenience API interaction (otherwise the SDK
 # implicitly creates a client that accesses the default server API host)
 lms.configure_default_client(LMS_API_HOST)
-model = lms.llm("lmstudio-community/gemma-3-1B-it-qat-GGUF")
+model = lms.llm(os.getenv('MODEL'))
 
 SYSTEM_PROMP = """You are a personal therapist.
 You will get messages from the user from past conversations.
 Evaluate them and followup on the last message.
+Respond with an acknowledgement and a followup question.
+
+Never user Markdown.
+Never more then 30 words.
+Do not greet in every new message.
 """
 
 def chat(history:list[Message]):
