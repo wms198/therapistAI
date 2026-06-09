920908c7f278c5050fca6ae1f6297793fa4e6763
wms198 <wang.minshi@icloud.com>
2026-06-09 14:04:43 +0200

    enables real-time, bidirectional communication between clients and servers

--- backend/therapistai/main.py
+++ backend/therapistai/main.py
@@ -6,6 +6,22 @@ from therapistai.db import engine
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from therapistai.routers import message, appointment, user, token
+import socketio
+
+# https://github.com/miguelgrinberg/python-socketio/blob/main/examples/server/asgi/fastapi-fiddle.py
+# https://github.com/BimaAdi/fastapi-with-python-socketio-example/blob/main/main.py
+sio = socketio.AsyncServer(
+    async_mode="asgi",
+    cors_allowed_origins="*"
+)
+@sio.on
+def ping(sid, data):
+    print('did a ping!')
+    print('ping', sid, data)
+
+@sio.on('messager')
+def userMessage(sid, data):
+    pass
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -19,9 +35,14 @@ async def lifespan(app: FastAPI):
 
 # https://fastapi.tiangolo.com/advanced/events/
 app = FastAPI(lifespan=lifespan)
+combined_app = socketio.ASGIApp(
+    socketio_server=sio,
+    other_asgi_app=app
+)
 
 origins = [
     "http://localhost:3000",
+    "http://localhost:5173",
 ]
 app.add_middleware(
     CORSMiddleware,
@@ -36,4 +57,3 @@ app.include_router(message.router)
 app.include_router(appointment.router)
 app.include_router(user.router)
 app.include_router(token.router)
-
